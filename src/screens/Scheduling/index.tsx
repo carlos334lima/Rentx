f467d8e98cf8cce11834e85f9b91d444d86200c2
Carlos Henrique de Lima <68131444+carlos334lima@users.noreply.github.com>
2021-07-20 21:53:57 -0300

    scheduling interface

--- src/screens/Scheduling/index.tsx
+++ src/screens/Scheduling/index.tsx
@@ -6,7 +6,6 @@ import { useTheme } from "styled-components";
 import { BackButton } from "../../components/BackButton";
 import { Button } from "../../components/Button";
 
-
 import ArrowSvg from "../../assets/arrow.svg";
 
 import {
@@ -20,25 +19,46 @@ import {
   DateValue,
   Content,
   Footer,
-} from './styles';
+} from "./styles";
 
 export function Scheduling() {
-
-  const theme = useTheme()
+  const theme = useTheme();
   return (
     <Container>
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
       <Header>
-      <Back>
-          <BackButton
-            color={theme.colors.shape}
-            onPress={() => {}}
-          />
+        <Back>
+          <BackButton color={theme.colors.shape} onPress={() => {}} />
         </Back>
 
         <Title>
-          Escolha uma {'\n'}data de início e {'\n'}fim do aluguel
+          Escolha uma {"\n"}data de início e {"\n"}fim do aluguel
         </Title>
+
+        <RentalPeriod>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue selected={true}>18/06/2021</DateValue>
+          </DateInfo>
+
+          <ArrowSvg />
+
+          <DateInfo>
+            <DateTitle>Até</DateTitle>
+            <DateValue selected={true}> 18/06/2021</DateValue>
+          </DateInfo>
+        </RentalPeriod>
       </Header>
+
+      <Content></Content>
+
+      <Footer>
+        <Button title="Confirmar"/>
+      </Footer>
     </Container>
-  )
+  );
 }
