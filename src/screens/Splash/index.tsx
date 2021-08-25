c455add2a8fe9cf8ffb7b3e579dcd5590f0ad8a1
Carlos Henrique de Lima <carlos334lima@gmail.com>
2021-08-24 22:33:37 -0300

    first animation

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,10 +1,35 @@
 import React from "react";
-import { StatusBar } from "react-native";
+import { Button, StatusBar, StyleSheet, Dimensions } from "react-native";
+
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  Easing
+} from "react-native-reanimated";
 
 //@Styles
 import { Container } from "./styles";
 
+const WIDTH = Dimensions.get("window").width;
+
 export function Splash() {
+  const animation = useSharedValue(0);
+
+  const AnimatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: withTiming(animation.value, {
+        duration: 4000,
+        easing: Easing.bounce
+
+      }) }],
+    };
+  });
+
+  function handleMove() {
+    animation.value = Math.random() * (WIDTH - 100);
+  }
+
   return (
     <Container>
       <StatusBar
@@ -12,6 +37,18 @@ export function Splash() {
         backgroundColor="transparent"
         translucent
       />
+
+      <Animated.View style={[styles.box, AnimatedStyle]} />
+
+      <Button title="mover" onPress={handleMove} />
     </Container>
   );
 }
+
+const styles = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: "red",
+  },
+});
