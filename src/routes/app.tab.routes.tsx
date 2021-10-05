fb2c5678d573b59a0866c5ec677380b42f8eb5c4
Carlos Henrique de Lima <carlos334lima@gmail.com>
2021-10-04 21:55:47 -0300

    tabBar finished

--- src/routes/app.tab.routes.tsx
+++ src/routes/app.tab.routes.tsx
@@ -3,19 +3,61 @@ import React from "react";
 //@libraries
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
+//@assets
+import HomeSvg from "../assets/home.svg";
+import PeopleSvg from "../assets/people.svg";
+import CarSvg from "../assets/car.svg";
+
 //@screens
 import { Home } from "../screens/Home";
 import { MyCars } from "../screens/MyCars";
 import { AppStackRoutes } from "./app.stack.routes";
+import theme from "../styles/theme";
+import { Platform } from "react-native";
 
 export function AppTabRoutes() {
   const { Navigator, Screen } = createBottomTabNavigator();
 
   return (
-    <Navigator>
-      <Screen name="Home" component={AppStackRoutes} />
-      <Screen name="Profile" component={Home} />
-      <Screen name="MyCars" component={MyCars} />
+    <Navigator
+      tabBarOptions={{
+        activeTintColor: theme.colors.main,
+        inactiveTintColor: theme.colors.text_detail,
+        showLabel: false,
+        style: {
+          paddingVertical: Platform.OS === "ios" ? 0 : 0,
+          height: 70,
+          backgroundColor: theme.colors.background_primary,
+        },
+      }}
+    >
+      <Screen
+        name="Home"
+        component={AppStackRoutes}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HomeSvg width={24} height={24} fill={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="Profile"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <PeopleSvg width={24} height={24} fill={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="MyCars"
+        component={MyCars}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <CarSvg width={24} height={24} fill={color} />
+          ),
+        }}
+      />
     </Navigator>
   );
 }
