8ad299f29482f98b53f1a483fb3e1e0fcd21a25c
Carlos Henrique de Lima <carlos334lima@gmail.com>
2021-10-16 20:56:46 -0300

    inputs

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   KeyboardAvoidingView,
   TouchableWithoutFeedback,
   Keyboard,
   Alert,
+  ScrollView,
 } from "react-native";
 
 //@libraries
@@ -44,37 +45,93 @@ import theme from "../../styles/theme";
 
 export function Profile() {
   const navigation = useNavigation();
+  const { user } = useAuth();
+
+  const [activeOption, setActiveOption] = useState<"dataEdit" | "passwordEdit">(
+    "dataEdit"
+  );
 
   function handleBack() {
     navigation.goBack();
   }
 
   return (
-    <Container>
-      <Header>
-        <HeaderTop>
-          <BackButton color={theme.colors.shape} onPress={handleBack} />
-          <HeaderTitle>Editar Perfil</HeaderTitle>
-          <LogoutButton onPress={() => {}}>
-            <Feather name="power" size={24} color={theme.colors.shape} />
-          </LogoutButton>
-        </HeaderTop>
+    <KeyboardAvoidingView behavior="position" enabled>
+      <Container>
+        <Header>
+          <HeaderTop>
+            <BackButton color={theme.colors.shape} onPress={handleBack} />
+            <HeaderTitle>Editar Perfil</HeaderTitle>
+            <LogoutButton onPress={() => {}}>
+              <Feather name="power" size={24} color={theme.colors.shape} />
+            </LogoutButton>
+          </HeaderTop>
 
-        <PhotoContainer>
-          <Photo
-            source={{
-              uri: "https://avatars.githubusercontent.com/u/68131444?v=4",
-            }}
-          />
-          <PhotoButton onPress={() => {}}>
-            <Feather 
-              name="camera"
-              size={24}
-              color={theme.colors.shape}
+          <PhotoContainer>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/68131444?v=4",
+              }}
             />
-          </PhotoButton>
-        </PhotoContainer>
-      </Header>
-    </Container>
+            <PhotoButton onPress={() => {}}>
+              <Feather name="camera" size={24} color={theme.colors.shape} />
+            </PhotoButton>
+          </PhotoContainer>
+        </Header>
+
+        <Content style={{ marginBottom: useBottomTabBarHeight() }}>
+          <Options>
+            <Option
+              active={activeOption === "dataEdit"}
+              onPress={() => setActiveOption("dataEdit")}
+            >
+              <OptionTitle active={activeOption === "dataEdit"}>
+                Dados
+              </OptionTitle>
+            </Option>
+            <Option
+              active={activeOption === "passwordEdit"}
+              onPress={() => setActiveOption("passwordEdit")}
+            >
+              <OptionTitle active={activeOption === "passwordEdit"}>
+                Trocar senha
+              </OptionTitle>
+            </Option>
+          </Options>
+
+          {activeOption === "dataEdit" && (
+            <Section>
+              <Input
+                iconName="user"
+                placeholder="Nome"
+                autoCorrect={false}
+                defaultValue={user.name}
+              />
+              <Input
+                iconName="mail"
+                editable={false}
+                autoCorrect={false}
+                defaultValue={user.email}
+              />
+              <Input
+                iconName="credit-card"
+                placeholder="CHN"
+                keyboardType="numeric"
+                autoCorrect={false}
+                defaultValue={user.driver_license}
+              />
+            </Section>
+          )}
+
+          {activeOption === "passwordEdit" && (
+            <Section>
+              <PasswordInput iconName="lock" placeholder="Senha atual" />
+              <PasswordInput iconName="lock" placeholder="Nova senha" />
+              <PasswordInput iconName="lock" placeholder="Repetir nova senha" />
+            </Section>
+          )}
+        </Content>
+      </Container>
+    </KeyboardAvoidingView>
   );
 }
